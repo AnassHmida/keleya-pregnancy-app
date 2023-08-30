3b7b89f128d95e8bfc8894924985e565385eff1a
Anes Hmida <33029571+AnassHmida@users.noreply.github.com>
2023-08-30 23:05:24 +0100

    add fall back language

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import 'intl-pluralrules'
 import {name as appName} from './app.json';
 import {initReactI18next} from 'react-i18next';
 import i18n from 'i18next';
 import * as RNLocalize from 'react-native-localize';
-
 import en from './src/translations/en';
 import de from './src/translations/de';
 
@@ -17,11 +17,13 @@ i18n.use(initReactI18next).init({
     en: {translation: en},
     de: {translation: de},
   },
-  compatibilityJSON: 'v3',
   lng: RNLocalize.getLocales()[0].languageCode,
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
 });
+
+
 
 AppRegistry.registerComponent(appName, () => App);
